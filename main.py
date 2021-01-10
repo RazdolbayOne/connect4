f630d759fbfff42a4da7600a03a2697371012278
RazdolbayOne <Yone312@gmail.com>
2021-01-10 20:38:18 +0200

    refactored some vars

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import pygame
 import sys
 import math
 
-
 # TODO NEEED TO REFACTOR ALL!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ROW_COUNT = 6  # Global Static Variable
 COLUMN_COUNT = 7  # These are the rows and colums specified before the program begins
 
-BLUE = (0, 0, 255)  # Its an RGB value (here only blue has some value)
-BLACK = (0, 0, 0)  # Black value in RGB is (0,0,0)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)  # YELLOW is a combination of both red and green
+BLUE_COLOR = (0, 0, 255)  # Its an RGB value (here only blue has some value)
+BLACK_COLOR = (0, 0, 0)  # Black value in RGB is (0,0,0)
+RED_COLOR = (255, 0, 0)
+YELLOW_COLOR = (255, 255, 0)  # YELLOW is a combination of both red and green
+SQUARE_SIZE = 100  # The value of each square is in pixels
 
+
+# def main():
+#    pass
 
 def create_board():
     board = np.zeros(
@@ -21,31 +24,36 @@ def create_board():
     return board
 
 
-def drop_piece(board, row, col, piece):
-    board[row][col] = piece  # fills the slot with the piece
+def drop_piece(board, row, col, chip):
+    """places chip on specific row and col pos in board"""
+    board[row][col] = chip  # fills the slot with the piece
 
 
-def is_valid_location(board, col):
-    return board[ROW_COUNT - 1][
-               col] == 0  # Checking to make sure that the column has not been filled fully, it checks the last row only (it returns a boolean value)
+def is_valid_col(board, col):
+    """Checking to make sure that the column has not been filled fully,
+     it checks the last row only (it returns a boolean value)"""
+    return board[ROW_COUNT - 1][col] == 0
 
 
-def get_next_open_row(board, col):
-    for r in range(ROW_COUNT):
-        if board[r][col] == 0:  # returns the slot which empty ,1st case which is empty
-            return r
+def get_valid_bottom_row_of_col(board, col):
+    """return most lower row of specific col"""
+    for row in range(ROW_COUNT):
+        if board[row][col] == 0:  # returns the slot which empty ,1st case which is empty
+            return row
 
 
 def print_board(board):
     """ To make the board build from the bottom up"""
     # 0 is the axis i.e. flip the board over x axis, right side up (np.flip() is a command in numpy)
-    print(np.flip(board,0))
+    print(np.flip(board, 0))
     print("---------------------------------------")
 
-def winning_move(board,piece):
-    # The game lets you know that 4 dots are in line(either horizontally,vertically and diagonally)
-    # Check horizontal locations for win
 
+def is_winning_state(board, piece):
+    """Check if after placing chip winning state  appiered
+       4 dots are in line(either horizontally,vertically and diagonally)
+    """
+    # Check horizontal locations for win
     for cols in range(COLUMN_COUNT - 3):  # we're subtracting 3 since the logic is applicable for 4 columns only
         for r in range(ROW_COUNT):
             if (board[r][cols] == piece and board[r][cols + 1] == piece and board[r][cols + 2] == piece and board[r][
@@ -77,96 +85,106 @@ def winning_move(board,piece):
 
 
 def draw_board(board):
-    """ just like print_board but it draws in pygame"""
+    """ draws in using pygame board"""
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
-            pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE,
-                                            SQUARESIZE))  # These are the (screen size,positon on y-axis,width,height)
-            pygame.draw.circle(screen, BLACK, (
-            int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)),
-                               RADIUS)  # pygame accepts only integers (refer pygame documentation)
+            pygame.draw.rect(game_window, BLUE_COLOR, (c * SQUARE_SIZE, r * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE,
+                                                       SQUARE_SIZE))  # These are the (screen size,positon on y-axis,width,height)
+            pygame.draw.circle(game_window, BLACK_COLOR, (
+                int(c * SQUARE_SIZE + SQUARE_SIZE / 2), int(r * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE / 2)),
+                               chip_radius)  # pygame accepts only integers (refer pygame documentation)
 
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             if board[r][c] == 1:
-                pygame.draw.circle(screen, RED, (
-                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(game_window, RED_COLOR, (
+                    int(c * SQUARE_SIZE + SQUARE_SIZE / 2), screen_height - int(r * SQUARE_SIZE + SQUARE_SIZE / 2)),
+                                   chip_radius)
             elif board[r][c] == 2:
-                pygame.draw.circle(screen, YELLOW, (
-                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(game_window, YELLOW_COLOR, (
+                    int(c * SQUARE_SIZE + SQUARE_SIZE / 2), screen_height - int(r * SQUARE_SIZE + SQUARE_SIZE / 2)),
+                                   chip_radius)
 
     pygame.display.update()  # so that the screen is updated after each click
 
 
 board = create_board()
 print_board(board)
-game_over = False
 turn = 0
 
-pygame.init()  # Done in every pygame project i.e. it initializes the pygame module
-
-SQUARESIZE = 100  # The value of each square is in pixels
+# every pygame project i.e. it initializes the pygame module
+pygame.init()
 
 # Defining the screen size
-width = COLUMN_COUNT * SQUARESIZE  # Its the number of columns times the squaresize
-height = (ROW_COUNT + 1) * SQUARESIZE  # One additional row for displaying the spot where we are dropping the circle
+screen_width = COLUMN_COUNT * SQUARE_SIZE  # Its the number of columns times the square_size
+screen_height = (ROW_COUNT + 1) * SQUARE_SIZE  # One additional row for displaying the spot where we are dropping the circle
 
-size = (width, height)  # size is a tuple of width and height
+size = (screen_width, screen_height)  # size is a tuple of width and height
 
-RADIUS = int(SQUARESIZE / 2 - 5)  # radius has to be a little smaller than the squaresize
+chip_radius = int(SQUARE_SIZE / 2 - 5)  # radius has to be a little smaller than the squaresize
 
-screen = pygame.display.set_mode(size)  # for pygame to read size  (from documentation)
+game_window = pygame.display.set_mode(size)  # for pygame to read size  (from documentation)
+
 draw_board(board)
-pygame.display.update()  # Whenever we want to update our display
 
-myfont = pygame.font.SysFont("monospace", 75)  # Refer the documentation ()
+# Whenever we want to update  display
+pygame.display.update()
 
+my_font = pygame.font.SysFont("monospace", 75)  # Refer the documentation ()
+game_over = False
 while not game_over:
     # pygame is an event based library it reads every key or mouse-click pressed as an event
     for event in pygame.event.get():
 
         pygame.display.update()
         if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-            posx = event.pos[0]  # This provides the position of the cursor
+            pygame.draw.rect(game_window, BLACK_COLOR, (0, 0, screen_width, SQUARE_SIZE))
+            mouse_click_pos_x = event.pos[0]  # This provides the position of the cursor
             if turn == 0:
-                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(game_window, RED_COLOR, (mouse_click_pos_x, int(SQUARE_SIZE / 2)), chip_radius)
             else:
-                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(game_window, YELLOW_COLOR, (mouse_click_pos_x, int(SQUARE_SIZE / 2)), chip_radius)
 
         if event.type == pygame.QUIT:  # Done in every game, it allows us to properly exit out of any game
             sys.exit()  # system exit (when we click on exit sign it should close)
 
-        if event.type == pygame.MOUSEBUTTONDOWN:  # Where we click it drops down the piece in that place
-            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+        # Where click it drops down the piece in that place
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.rect(game_window, BLACK_COLOR, (0, 0, screen_width, SQUARE_SIZE))
 
             # Ask for Player 1 Input
             if (turn == 0):
-                posx = event.pos[0]  # the initial position
-                col = int(math.floor(posx / SQUARESIZE))  # The player enters the column no. here
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
+                mouse_click_pos_x = event.pos[0]  # the initial position
+                col = int(math.floor(mouse_click_pos_x / SQUARE_SIZE))  # The player enters the column no. here
+                if is_valid_col(board, col):
+                    row = get_valid_bottom_row_of_col(board, col)
                     drop_piece(board, row, col, 1)
 
-                    if winning_move(board, 1):
-                        label = myfont.render("Player 1 wins!!!", 1, RED)  # The font will be printed in "red"
-                        screen.blit(label, (40, 10))  # It updates text to the specific part(position) of the screen
+                    if is_winning_state(board, 1):
+                        label = my_font.render("Player 1 wins!!!", 1, RED_COLOR)  # The font will be printed in "red"
+                        game_window.blit(label,
+                                         (40, 10))  # It updates text to the specific part(position) of the screen
                         game_over = True  # It ends the game
 
 
             # Ask for Player 2 Input
             else:
-                posx = event.pos[0]  # the initial position
-                col = int(math.floor(posx / SQUARESIZE))  # The player enters the column no. here
+                # the initial position
+                mouse_click_pos_x = event.pos[0]
+                # calc in which col chip will be placed
+                col = int(math.floor(mouse_click_pos_x / SQUARE_SIZE))
 
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
+                # check if it possible to place in colon the  chip
+                if is_valid_col(board, col):
+                    # get most_valid_bottom row of colomn
+                    row = get_valid_bottom_row_of_col(board, col)
                     drop_piece(board, row, col, 2)
 
-                    if winning_move(board, 2):
-                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
-                        screen.blit(label, (40, 10))
+                    if is_winning_state(board, 2):
+                        label = my_font.render("Player 2 AI wins!!", 1, YELLOW_COLOR)
+                        game_window.blit(label, (40, 10))
                         game_over = True  # It ends the game
+
             print_board(board)
             draw_board(board)  # we need to draw_board after every turn
 
@@ -174,4 +192,8 @@ while not game_over:
             turn = turn % 2  # It alternates between player 1 and player 2
 
         if game_over:
-           pygame.time.wait(3000)  # The wait value is in millisecond hence here the wait is 3 seconds
+            pygame.time.wait(3000)  # The wait value is in millisecond hence here the wait is 3 seconds
+
+# if __name__ == "__main__":
+# execute only if run as a script
+#    main()
